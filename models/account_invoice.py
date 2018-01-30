6e84c899bd3d3b7413183704b3eccd55c36b121f
Nay3li <nayeli.valencia@xmarts.com.mx>
2018-01-30 10:15:06 -0600

    ajuste solo a total y amount untaxed de la factura

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -10,20 +10,22 @@ class AccountInvoice(models.Model):
     @api.multi
     def action_setting(self):
         for invoice in self:
-            for line in invoice.tax_line_ids:
-                amount= line.amount_total
-                valor = round(amount, 2)
-                line.write({'amount_total': valor})
-                line.write({'amount': valor})
-            for l in invoice.invoice_line_ids:
-                redondeo = round(l.price_unit, 2)
-                l.write({'price_unit': redondeo})
+            #for line in invoice.tax_line_ids:
+            #3    amount= line.amount_total
+            #    valor = round(amount, 2)
+            #    line.write({'amount_total': valor})
+             #   line.write({'amount': valor})
+            #for l in invoice.invoice_line_ids:
+            #    redondeo = round(l.price_unit, 2)
+            #    l.write({'price_unit': redondeo})
             amount_untax_redondeo= round(invoice.amount_untaxed,2)
             invoice.write({'amount_untaxed': amount_untax_redondeo})
-            amount_tax_redondeo = round(invoice.amount_tax, 2)
-            invoice.write({'amount_tax': amount_tax_redondeo})
+            #amount_tax_redondeo = round(invoice.amount_tax, 2)
+            #invoice.write({'amount_tax': amount_tax_redondeo})
             amount_total_redondeo = round(invoice.amount_total, 2)
             invoice.write({'amount_total': amount_total_redondeo})
+            amount_total_signed_redondeo = round(invoice.amount_total_signed, 2)
+            invoice.write({'amount_total_signed': amount_total_signed_redondeo})
             residual_redondeo = round(invoice.residual, 2)
             invoice.write({'residual': residual_redondeo})
             invoice_tax = self.env['account.invoice.tax'].search([('invoice_id','=',invoice.id)])
