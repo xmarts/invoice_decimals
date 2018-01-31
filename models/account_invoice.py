897c91c0977b26f5732e0c2e6583c00ca0520b73
Nay3li <nayeli.valencia@xmarts.com.mx>
2018-01-31 13:06:24 -0600

    ajuste de taxes

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -10,28 +10,29 @@ class AccountInvoice(models.Model):
     @api.multi
     def action_setting(self):
         for invoice in self:
-            for line in invoice.tax_line_ids:
-                amount= line.amount_total
-                valor = round(amount, 2)
-                line.write({'amount_total': valor})
-                line.write({'amount': valor})
+            #for line in invoice.tax_line_ids:
+            #    amount= line.amount_total
+            #    valor = round(amount, 2)
+            #    line.write({'amount_total': valor})
             for l in invoice.invoice_line_ids:
             #    redondeo = round(l.price_unit, 2)
             #    l.write({'price_unit': redondeo})
                 redondeo = round(l.price_subtotal, 2)
                 l.write({'price_subtotal': redondeo})
+                for t in  l.invoice_line_tax_ids:
+                    monto= t.amount_total
             #amount_untax_redondeo= round(invoice.amount_untaxed,2)
             #invoice.write({'amount_untaxed': amount_untax_redondeo})
-            amount_tax_redondeo = round(invoice.amount_tax, 2)
-            invoice.write({'amount_tax': amount_tax_redondeo})
+            #amount_tax_redondeo = round(invoice.amount_tax, 2)
+            #invoice.write({'amount_tax': amount_tax_redondeo})
             #amount_total_redondeo = round(invoice.amount_total, 2)
             #invoice.write({'amount_total': amount_total_redondeo})
             #amount_total_signed_redondeo = round(invoice.amount_total_signed, 2)
             #invoice.write({'amount_total_signed': amount_total_signed_redondeo})
             #residual_redondeo = round(invoice.residual, 2)
             #invoice.write({'residual': residual_redondeo})
-            invoice_tax = self.env['account.invoice.tax'].search([('invoice_id','=',invoice.id)])
-            for i in invoice_tax:
-                x_redondeo = round(i.amount_total, 2)
-                i.write({'amount_total': x_redondeo})
+            #invoice_tax = self.env['account.invoice.tax'].search([('invoice_id','=',invoice.id)])
+            #for i in invoice_tax:
+            #    x_redondeo = round(i.amount_total, 2)
+            #    i.write({'amount_total': x_redondeo})
 
